f6c735d70d1b90594e84781bd61231375199c20e
kaelspencer <kael@euler>
2013-05-14 22:18:27 -0700

    Add Caching Cache all responses with memcached.

--- controllers.py
+++ controllers.py
@@ -1,10 +1,13 @@
 from app import app
 from db import get_db
-from flask import abort, Response, jsonify
+from flask import abort, request, Response, jsonify
+from werkzeug.contrib.cache import MemcachedCache
 from functools import wraps
 from helpers import sys_to_id, sta_to_sysid, staid_to_sysid, sysid_list_to_object
 from graph import SystemGraph
 import json
+
+cache = MemcachedCache(['127.0.0.1:11211'])
 
 # This decorator will return a 404 if the queries return no results.
 def handleLookupError(f):
@@ -14,6 +17,18 @@ def handleLookupError(f):
             return f(*args, **kwargs)
         except LookupError:
             abort(404)
+    return decorated_function
+
+def cached(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        cache_key = request.path.replace(' ', '_')
+        rv = cache.get(cache_key)
+        if rv is not None:
+            return rv
+        rv = f(*args, **kwargs)
+        cache.set(cache_key, rv)
+        return rv
     return decorated_function
 
 @app.route('/')
@@ -27,6 +42,8 @@ def index():
 # This set of methods handles the routes. Ints are needed, but strings can be supplied.
 # The route method is appended with two letters that describe the type of parameters.
 @app.route('/route/<int:source>/<int:destination>/')
+@handleLookupError
+@cached
 def route_ii(source, destination):
     g = SystemGraph()
     route = g.route(source, destination)
@@ -36,18 +53,21 @@ def route_ii(source, destination):
 
 @app.route('/route/<source>/<int:destination>/')
 @handleLookupError
+@cached
 def route_si(source, destination):
     i_source = sys_to_id(source)
     return route_ii(i_source, destination)
 
 @app.route('/route/<int:source>/<destination>/')
 @handleLookupError
+@cached
 def route_is(source, destination):
     i_destination = sys_to_id(destination)
     return route_ii(source, i_destination)
 
 @app.route('/route/<source>/<destination>/')
 @handleLookupError
+@cached
 def route_ss(source, destination):
     i_source = sys_to_id(source)
     i_destination = sys_to_id(destination)
@@ -56,6 +76,7 @@ def route_ss(source, destination):
 # Same as the above methods, except the source is a station name or ID.
 @app.route('/route/station/<int:source>/<int:destination>/')
 @handleLookupError
+@cached
 def route_station_ii(source, destination):
     sysid_source = staid_to_sysid(source)
     sysid_destination = staid_to_sysid(destination)
@@ -63,6 +84,7 @@ def route_station_ii(source, destination):
 
 @app.route('/route/station/<source>/<int:destination>/')
 @handleLookupError
+@cached
 def route_station_si(source, destination):
     sysid_source = sta_to_sysid(source)
     sysid_destination = staid_to_sysid(destination)
@@ -70,6 +92,7 @@ def route_station_si(source, destination):
 
 @app.route('/route/station/<int:source>/<destination>/')
 @handleLookupError
+@cached
 def route_station_is(source, destination):
     sysid_source = staid_to_sysid(source)
     sysid_destination = sta_to_sysid(destination)
@@ -77,6 +100,7 @@ def route_station_is(source, destination):
 
 @app.route('/route/station/<source>/<destination>/')
 @handleLookupError
+@cached
 def route_station_ss(source, destination):
     sysid_source = sta_to_sysid(source)
     sysid_destination = sta_to_sysid(destination)
@@ -85,6 +109,8 @@ def route_station_ss(source, destination):
 # This set of methods handles the jump counts. Ints are needed, but strings can be supplied.
 # The route method is appended with two letters that describe the type of parameters.
 @app.route('/jump/<int:source>/<int:destination>/')
+@handleLookupError
+@cached
 def jump_ii(source, destination):
     g = SystemGraph()
     jumps = g.distance(source, destination)
@@ -92,18 +118,21 @@ def jump_ii(source, destination):
 
 @app.route('/jump/<source>/<int:destination>/')
 @handleLookupError
+@cached
 def jump_si(source, destination):
     i_source = sys_to_id(source)
     return jump_ii(i_source, destination)
 
 @app.route('/jump/<int:source>/<destination>/')
 @handleLookupError
+@cached
 def jump_is(source, destination):
     i_destination = sys_to_id(destination)
     return jump_ii(source, i_destination)
 
 @app.route('/jump/<source>/<destination>/')
 @handleLookupError
+@cached
 def jump_ss(source, destination):
     i_source = sys_to_id(source)
     i_destination = sys_to_id(destination)
@@ -112,6 +141,7 @@ def jump_ss(source, destination):
 # Same as the above methods, except the source is a station name or ID.
 @app.route('/jump/station/<int:source>/<int:destination>/')
 @handleLookupError
+@cached
 def jump_station_ii(source, destination):
     sysid_source = staid_to_sysid(source)
     sysid_destination = staid_to_sysid(destination)
@@ -119,6 +149,7 @@ def jump_station_ii(source, destination):
 
 @app.route('/jump/station/<source>/<int:destination>/')
 @handleLookupError
+@cached
 def jump_station_si(source, destination):
     sysid_source = sta_to_sysid(source)
     sysid_destination = staid_to_sysid(destination)
@@ -126,6 +157,7 @@ def jump_station_si(source, destination):
 
 @app.route('/jump/station/<int:source>/<destination>/')
 @handleLookupError
+@cached
 def jump_station_is(source, destination):
     sysid_source = staid_to_sysid(source)
     sysid_destination = sta_to_sysid(destination)
@@ -133,6 +165,7 @@ def jump_station_is(source, destination):
 
 @app.route('/jump/station/<source>/<destination>/')
 @handleLookupError
+@cached
 def jump_station_ss(source, destination):
     sysid_source = sta_to_sysid(source)
     sysid_destination = sta_to_sysid(destination)
