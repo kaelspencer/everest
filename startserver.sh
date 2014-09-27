8c9b9c61a515784b1d84ba1e168a0a0bcd8c9ca3
Kael Spencer <kaelspencer@gmail.com>
2014-09-27 23:25:15 +0000

    Update Startup Script for Supervisord

--- startserver.sh
+++ startserver.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# Replace these three settings.
-PROJDIR=$PWD
-PIDFILE="$PROJDIR/everest.pid"
-
-if [ -f $PIDFILE ]; then
-    kill `cat -- $PIDFILE`
-    rm -f -- $PIDFILE
-fi
-
 . ../virtualenv/everest/bin/activate
 
 export EVEREST_CONFIG=production.cfg
-exec uwsgi -s 127.0.0.1:49152 --module everest --callable app --daemonize /var/log/uwsgi/everest.log --pidfile $PIDFILE
+exec uwsgi -s 127.0.0.1:49152 --module everest --callable app --logto /var/log/uwsgi/everest.log
